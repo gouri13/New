e251edd98933b5d544ec94d9fc508a265fe5994c
gouribodade <gouribodade@DESKTOP-VEP0PEJ>
2021-08-17 11:34:54 +0530

    Fron New Laptop

---
+++ src/pkg1/Hello.java
@@ -0,0 +1,20 @@
+package pkg1;
+import java.util.*;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter string");
+		String str= sc.nextLine(); 
+		
+		System.out.println("enter integer");
+		int a=sc.nextInt();
+		
+		System.out.println("enter float");
+		float b=sc.nextFloat();
+		System.out.println(a+str+b);
+		
+	}
+
+}
